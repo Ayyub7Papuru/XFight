55e2893a8762296dcee2a6e10142c2b1c9702cc6
HanAyyub <han7ayyub@gmail.com>
2019-01-10 19:14:40 +0100

    Added player and team class

---
+++ XFight/Team.swift
@@ -0,0 +1,20 @@
+//
+//  Team.swift
+//  XFight
+//
+//  Created by SayajinPapuru on 10/01/2019.
+//  Copyright © 2019 HanAyyubInc. All rights reserved.
+//
+
+import Foundation
+
+class Team {
+    var teamName: String
+    var characterName: String
+    
+    init(teamName: String, characterName: String) {
+        self.teamName = teamName
+        self.characterName = characterName
+        
+    }
+}
